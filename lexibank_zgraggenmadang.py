1474ca235ee5a5ef21b9b20dec7837899bf42fa1
Christoph Rzymski <christoph@functor-argument.net>
2019-07-03 13:46:00 +0200

    Code cleanup, Glottolog 4.0., release 2.0.1.

--- lexibank_zgraggenmadang.py
+++ lexibank_zgraggenmadang.py
@@ -1,13 +1,8 @@
-# coding=utf-8
-from __future__ import unicode_literals, print_function
-import re
-
 import attr
-from clldutils.path import Path
-from pylexibank.dataset import Metadata
 from clldutils.misc import slug
+from clldutils.path import Path
 from pylexibank.dataset import Dataset as BaseDataset, Language
-from pylexibank.util import getEvoBibAsBibtex, pb
+from pylexibank.util import getEvoBibAsBibtex
 
 
 @attr.s
@@ -17,15 +12,14 @@ class Variety(Language):
 
 class Dataset(BaseDataset):
     dir = Path(__file__).parent
-    id = 'zgraggenmadang'
+    id = "zgraggenmadang"
     language_class = Variety
 
     def cmd_install(self, **kw):
-        #wl_data = {}
         wl_data = []
-        log = kw['log']
-        log.info('building wordlist ...')
-        for idx, row in enumerate(self.raw.read_tsv('madang.csv')):
+        log = kw["log"]
+        log.info("building wordlist ...")
+        for idx, row in enumerate(self.raw.read_tsv("madang.csv")):
             if idx > 1:
                 # get fields for the entry, correcting it if necessary
                 _, doculect, concept, counterpart = row
@@ -33,47 +27,54 @@ class Dataset(BaseDataset):
                     counterpart = self.lexemes[counterpart]
 
                 # add to wordlist data
-                wl_data.append({
-                    'doculect' : doculect,
-                    'concept' : concept,
-                    'counterpart' : counterpart,
-                })
+                wl_data.append(
+                    {"doculect": doculect, "concept": concept, "counterpart": counterpart}
+                )
 
-        log.info('... data assembled ...')
+        log.info("... data assembled ...")
 
         # build CLDF data
         with self.cldf as ds:
             ds.add_sources()
 
             # add languages, and build dictionary of sources
-            ds.add_languages(id_factory=lambda l: l['Name'])
-            lang_source = {l['Name']: l['Source'] for l in self.languages}
+            ds.add_languages(id_factory=lambda l: l["Name"])
+            lang_source = {l["Name"]: l["Source"] for l in self.languages}
 
             for concept in self.concepts:
                 ds.add_concept(
-                        ID=slug(concept['ENGLISH']),
-                        Concepticon_ID=concept['CONCEPTICON_ID'],
-                        Name=concept['ENGLISH']
-                        )
+                    ID=slug(concept["ENGLISH"]),
+                    Concepticon_ID=concept["CONCEPTICON_ID"],
+                    Name=concept["ENGLISH"],
+                )
 
             for concept in self.conceptlist.concepts.values():
                 ds.add_concept(
                     ID=slug(concept.english),
                     Concepticon_ID=concept.concepticon_id,
-                    Name=concept.concepticon_gloss
+                    Name=concept.concepticon_gloss,
                 )
 
             # add lexemes
             for row in wl_data:
                 ds.add_lexemes(
-                    Language_ID=row['doculect'],
-                    Parameter_ID=slug(row['concept']),
-                    Value=row['counterpart'],
-                    Source=lang_source[row['doculect']],
+                    Language_ID=row["doculect"],
+                    Parameter_ID=slug(row["concept"]),
+                    Value=row["counterpart"],
+                    Source=lang_source[row["doculect"]],
                 )
-
 
     def cmd_download(self, **kw):
         if not self.raw.exists():
             self.raw.mkdir()
-        self.raw.write('sources.bib', getEvoBibAsBibtex('Zgraggen1980NA', 'Zgraggen1980RC', 'Zgraggen1980SA', 'Zgraggen1980MA', 'List2014e', **kw))
+        self.raw.write(
+            "sources.bib",
+            getEvoBibAsBibtex(
+                "Zgraggen1980NA",
+                "Zgraggen1980RC",
+                "Zgraggen1980SA",
+                "Zgraggen1980MA",
+                "List2014e",
+                **kw
+            ),
+        )
--- test.py
+++ test.py
@@ -1,8 +1,3 @@
-# coding: utf-8
-from __future__ import unicode_literals
-
-
 def test_valid(cldf_dataset, cldf_logger):
     assert any(r['Value'] == 'ezandə kɨ-' for r in cldf_dataset['FormTable'])
     assert cldf_dataset.validate(log=cldf_logger)
-
